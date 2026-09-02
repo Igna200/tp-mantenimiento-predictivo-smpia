fddf06ecfc6d5d714433b3a7597be27721dc3ad0
UrianZ <uzbikoski@itba.edu.ar>
2026-09-02 13:29:40 -0300

    Cambios menores

---
+++ SIMPIA.py
@@ -0,0 +1,5 @@
+class SIMPIA:
+    def __init__(self, historial_avisos):       #Nos conviene que sea un tipo de dato lista o un tipo de dato aviso ??
+        self.historial_avisos=historial_avisos
+
+    
